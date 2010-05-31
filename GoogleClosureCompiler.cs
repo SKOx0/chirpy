ade85905b20097440bc148ee91bcff5c65b5ad33
Evan Nagle <evan.nagle@gmail.com>
2010-05-30 20:26:54 -1000

    Committing all project files.

---
+++ GoogleClosureCompiler.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Web;
+using System.Xml;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Yahoo.Yui.Compressor;
+
+namespace Zippy.Chirp
+{
+    public class GoogleClosureCompiler
+    {
+
+        private const string PostData = "js_code={0}&output_format=xml&output_info=compiled_code&compilation_level={1}";
+        private const string ApiEndpoint = "http://closure-compiler.appspot.com/compile";
+
+        /// <summary>
+        /// Compresses the specified file using Google's Closure Compiler algorithm.
+        /// <remarks>
+        /// The file to compress must be smaller than 200 kilobytes.
+        /// </remarks>
+        /// </summary>
+        /// <param name="js">javascript to compiler.</param>
+        /// <param name="compressMode">SIMPLE_OPTIMIZATIONS,WHITESPACE_ONLY,ADVANCED_OPTIMIZATIONS</param>
+        /// <returns>A compressed version of the specified JavaScript file.</returns>
+        public static string Compress(string fileName,  string compressMode)
+        {
+            if (!File.Exists(fileName)) throw new Exception("File does not exist: " + fileName);
+
+            string js = File.ReadAllText(fileName);
+            if (string.IsNullOrEmpty(js)) return string.Empty;
+
+            try
+            {
+                long size = new FileInfo(fileName).Length;
+                if (size < 160000)
+                {
+                    if (string.IsNullOrEmpty(compressMode)) compressMode = "SIMPLE_OPTIMIZATIONS";
+
+                    //string source = File.ReadAllText(file);
+                    XmlDocument xml = CallApi(js, compressMode);
+
+                    return xml.SelectSingleNode("//compiledCode").InnerText;
+                }
+                else
+                {
+                    //file too large
+                    string exceptionMessage = "//file size too large for Google: " + size.ToString("#,#") + Environment.NewLine;
+                    return exceptionMessage + JavaScriptCompressor.Compress(js);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Translation failed.");
+                return js;
+            }
+        }
+
+
+        /// <summary>
+        /// Calls the API with the source file as post data.
+        /// </summary>
+        /// <param name="source">The content of the source file.</param>
+        /// <returns>The Xml response from the Google API.</returns>
+        private static XmlDocument CallApi(string source, string compressMode)
+        {
+            //http://code.google.com/intl/fr-CA/closure/compiler/docs/api-ref.html
+
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("content-type", "application/x-www-form-urlencoded");
+                string data = string.Format(PostData, HttpUtility.UrlEncode(source), compressMode);
+                string result = client.UploadString(ApiEndpoint, data);
+
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(result);
+                return doc;
+            }
+        }
+    }
+}
---
+++ Xml/FileGroupXml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+
+namespace Zippy.Chirp.Xml
+{
+    public class FileGroupXml
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public IList<FileXml> Files { get; set; }
+
+        public FileGroupXml(XElement xElement) : this(xElement, string.Empty) { }
+        public FileGroupXml(XElement xElement, string basePath)
+        {
+            var name = xElement.Attribute("Name");
+
+            if (name == null)
+            {
+                throw new Exception("Name attribute required on FileGroup element");
+            }
+
+            Name = name.Value;
+            Path = System.IO.Path.Combine(basePath, Name);
+
+            var files = xElement.Descendants("File")
+                .Select(n => new FileXml(n, basePath));
+
+            var folderFiles = xElement.Descendants("Folder")
+                .Select(n => new FolderXml(n, basePath))
+                .SelectMany(n => n.FileXmlList);
+
+            Files = files.Union(folderFiles).ToList();
+        }
+    }
+}
