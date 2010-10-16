79b9ee96db319cae30a3ba045152378211a93a9e
ploufs <ploufs@localhost>
2010-10-16 18:36:15 -0400

    Console application support .config file.

--- Console.Chirp/Program.cs
+++ Console.Chirp/Program.cs
@@ -12,12 +12,12 @@ namespace Console.Chirp
 
         static void Main(string[] args)
         {
-            string findPath =string.Empty ;
+            string findPath = string.Empty;
             if (args.Length > 0) findPath = args[0];
 
             Settings.Load();
-            
-          
+
+
             List<TransformEngine> listTrasformEngine = new List<TransformEngine>();
 
             listTrasformEngine.Add(new YuiCssEngine());
@@ -38,12 +38,31 @@ namespace Console.Chirp
                     {
                         string text = System.IO.File.ReadAllText(filename);
                         string minFileName = Utilities.GetBaseFileName(filename, extension) + transformEngine.GetOutputExtension(filename);
-                       text= transformEngine.Transform( filename, text,null);
-                       System.IO.File.WriteAllText(minFileName, text);
-                       System.Console.WriteLine(string.Format("{0} -- {1}", transformEngine.GetType().Name, filename));
+                        text = transformEngine.Transform(filename, text, null);
+                        System.IO.File.WriteAllText(minFileName, text);
+                        System.Console.WriteLine(string.Format("{0} -- {1}", transformEngine.GetType().Name, filename));
                     }
                 }
             }
+
+            //config file
+            var configEngine = new ConfigEngine();
+
+            foreach (string filename in Directory.GetFiles(findPath, "*" + Settings.ChirpConfigFile, SearchOption.AllDirectories))
+            {
+                try
+                {
+                    System.Console.WriteLine(string.Format("ConfigEngine -- {0}",  filename));
+                    configEngine.Run(filename, null);
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    System.Console.WriteLine(string.Format("File not found in config file={0}",filename));
+                }
+                
+            }
+
+
         }
     }
 }
