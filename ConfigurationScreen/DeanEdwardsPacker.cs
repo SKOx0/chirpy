358e9c5d77d0f105a8a85866c397d8407b6c5644
ploufs <ploufs@localhost>
2012-01-15 17:50:40 -0500

    Add config screen for Dean Edwards packer Add missing file in last check-in.

---
+++ ConfigurationScreen/DeanEdwardsPacker.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Zippy.Chirp.ConfigurationScreen
+{
+    public partial class DeanEdwardsPacker : BaseConfigurationControl
+    {
+        public DeanEdwardsPacker()
+        {
+            InitializeComponent();
+        }
+
+        public override void OnAfterCreated(EnvDTE.DTE dteObject)
+        {
+            this.cboPackerEncoding.DataSource = System.Enum.GetNames(typeof(Dean.Edwards.ECMAScriptPacker.PackerEncoding));
+            this.cboPackerEncoding.SelectedItem = this.Settings.ChirpDeanEdwardsPackerEncoding;
+            this.chkFastDecode.Checked = this.Settings.ChirpDeanEdwardsPackerFastDecode;
+            this.chkSpecialChars.Checked = this.Settings.ChirpDeanEdwardsPackerSpecialChars;
+        }
+
+        public override void OnOK()
+        {
+            this.Settings.ChirpDeanEdwardsPackerEncoding = (Dean.Edwards.ECMAScriptPacker.PackerEncoding)System.Enum.Parse(typeof(Dean.Edwards.ECMAScriptPacker.PackerEncoding), this.cboPackerEncoding.SelectedItem.ToString());
+            this.Settings.ChirpDeanEdwardsPackerFastDecode = chkFastDecode.Checked;
+            this.Settings.ChirpDeanEdwardsPackerSpecialChars = chkSpecialChars.Checked;
+            this.Settings.Save();
+
+        }
+
+        private void linkLabelModeInfo_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://dean.edwards.name/packer/");
+        }
+    }
+}
---
+++ Zippy.Chirp.Tests/Engines/YuiJsEngineTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zippy.Chirp.Tests.Engines
+{
+    [TestClass]
+    public class YuiJsEngineTest:BaseTest
+    {
+        private void YuiJsDefaultSetting()
+        {
+            Settings setting = Settings.Instance();
+            setting.YuiJsSettings.DisableOptimizations = false;
+            setting.YuiJsSettings.IsObfuscateJavascript = false;
+            setting.YuiJsSettings.LineBreakPosition = 0;
+            setting.YuiJsSettings.PreserveAllSemiColons = false;
+            setting.Save();
+        }
+
+        [TestMethod]
+        public void TestYuiJsEngine()
+        {
+            this.YuiJsDefaultSetting();
+            string code = "if(test) {\r\n\t alert('test'); }";
+            code = TestEngine<Zippy.Chirp.Engines.YuiJsEngine>("c:\\test.js", code);
+
+            Assert.IsTrue(code == "if(test){alert(\"test\")};" || code == "if(test){alert(\"test\")\n};");
+        }
+
+        [TestMethod]
+        public void TestYuiJsEngineThrowTaskListErrorOnJsError()
+        {
+            this.YuiJsDefaultSetting();
+            TaskList.Instance.RemoveAll();
+            string code = "if(test  }";
+            code = TestEngine<Zippy.Chirp.Engines.YuiJsEngine>("c:\\test.js", code);
+
+            Assert.AreEqual(TaskList.Instance.Errors.Count(), 1);
+        }
+    }
+}
