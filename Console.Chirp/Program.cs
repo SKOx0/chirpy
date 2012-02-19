bf8222118be205b68bc9200c748ff09125e32635
ploufs <ploufs@localhost>
2012-02-18 23:21:43 -0500

    fix bug in console application don't mash file on root directory http://chirpy.codeplex.com/discussions/303318

--- Console.Chirp/Program.cs
+++ Console.Chirp/Program.cs
@@ -15,69 +15,85 @@ namespace Console.Chirp
                 findPath = args[0];
             }
 
-            List<TransformEngine> listTrasformEngine = new List<TransformEngine>();
-
-            listTrasformEngine.Add(new DeanEdwardsPackerEngine());
-            listTrasformEngine.Add(new YuiCssEngine());
-            listTrasformEngine.Add(new YuiJsEngine());
-            listTrasformEngine.Add(new ClosureCompilerEngine());
-            listTrasformEngine.Add(new LessEngine());
-            listTrasformEngine.Add(new SassEngine());
-            listTrasformEngine.Add(new CoffeeScriptEngine());
-            listTrasformEngine.Add(new UglifyEngine());
-            listTrasformEngine.Add(new MsJsEngine());
-            listTrasformEngine.Add(new MsCssEngine());
-            //// listTrasformEngine.Add(new ConfigEngine());
-            listTrasformEngine.Add(new ViewEngine());
-            //// listTrasformEngine.Add(new T4Engine());
-
-            foreach (var directory in Directory.GetDirectories(findPath, "*", SearchOption.AllDirectories))
-            {
-                var settings = new Settings(directory);
-                foreach (TransformEngine transformEngine in listTrasformEngine)
-                {
-                    transformEngine.Settings = settings;
-                    foreach (string extension in transformEngine.Extensions)
-                    {
-                        foreach (
-                            string filename in
-                                Directory.GetFiles(directory, "*" + extension, SearchOption.TopDirectoryOnly))
-                        {
-                            if (filename.Contains(".min."))
-                            {
-                                continue;
-                            }
-
-                            string text = System.IO.File.ReadAllText(filename);
-                            string minFileName = Utilities.GetBaseFileName(filename, extension) +
-                                                 transformEngine.GetOutputExtension(filename);
-                            text = transformEngine.Transform(filename, text, null);
-                            System.IO.File.WriteAllText(minFileName, text);
-                            System.Console.WriteLine(string.Format("{0} -- {1}", transformEngine.GetType().Name, filename));
-                        }
-                    }
-                }
-            }
-
+           
             // config file
-            var configEngine = new ConfigEngine();
-
+            ConfigDirectory(findPath);
+            EngineDirectory(findPath);
             foreach (var directory in Directory.GetDirectories(findPath, "*", SearchOption.AllDirectories))
             {
-                var settings = new Settings(directory);
-                foreach (string filename in Directory.GetFiles(directory, "*" + new Settings(directory).ChirpConfigFile, SearchOption.TopDirectoryOnly))
+                ConfigDirectory(directory);
+                EngineDirectory(directory);
+            }
+        }
+
+        /// <summary>
+        /// process directory to config engine
+        /// </summary>
+        /// <param name="directoryPath">Directory full path</param>
+       static void ConfigDirectory(string directoryPath)
+        {
+            var configEngine = new ConfigEngine();
+            var settings = new Settings(directoryPath);
+            foreach (string filename in Directory.GetFiles(directoryPath, "*" + settings.ChirpConfigFile, SearchOption.TopDirectoryOnly))
+            {
+                try
                 {
-                    try
-                    {
-                        System.Console.WriteLine(string.Format("ConfigEngine -- {0}", filename));
-                        configEngine.Run(filename, null);
-                    }
-                    catch (System.IO.FileNotFoundException)
-                    {
-                        System.Console.WriteLine(string.Format("File not found in config file={0}", filename));
-                    }
+                    System.Console.WriteLine(string.Format("ConfigEngine -- {0}", filename));
+                    configEngine.Run(filename, null);
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    System.Console.WriteLine(string.Format("File not found in config file={0}", filename));
                 }
             }
         }
+
+        /// <summary>
+        /// Process directory in all engine
+        /// </summary>
+       /// <param name="directoryPath">Directory full path</param>
+       static void EngineDirectory(string directoryPath)
+       {
+           List<TransformEngine> listTrasformEngine = new List<TransformEngine>();
+
+           listTrasformEngine.Add(new DeanEdwardsPackerEngine());
+           listTrasformEngine.Add(new YuiCssEngine());
+           listTrasformEngine.Add(new YuiJsEngine());
+           listTrasformEngine.Add(new ClosureCompilerEngine());
+           listTrasformEngine.Add(new LessEngine());
+           listTrasformEngine.Add(new SassEngine());
+           listTrasformEngine.Add(new CoffeeScriptEngine());
+           listTrasformEngine.Add(new UglifyEngine());
+           listTrasformEngine.Add(new MsJsEngine());
+           listTrasformEngine.Add(new MsCssEngine());
+           //// listTrasformEngine.Add(new ConfigEngine());
+           listTrasformEngine.Add(new ViewEngine());
+           //// listTrasformEngine.Add(new T4Engine());
+
+           var settings = new Settings(directoryPath);
+           foreach (TransformEngine transformEngine in listTrasformEngine)
+           {
+               transformEngine.Settings = settings;
+               foreach (string extension in transformEngine.Extensions)
+               {
+                   foreach (
+                       string filename in
+                           Directory.GetFiles(directoryPath, "*" + extension, SearchOption.TopDirectoryOnly))
+                   {
+                       if (filename.Contains(".min."))
+                       {
+                           continue;
+                       }
+
+                       string text = System.IO.File.ReadAllText(filename);
+                       string minFileName = Utilities.GetBaseFileName(filename, extension) +
+                                            transformEngine.GetOutputExtension(filename);
+                       text = transformEngine.Transform(filename, text, null);
+                       System.IO.File.WriteAllText(minFileName, text);
+                       System.Console.WriteLine(string.Format("{0} -- {1}", transformEngine.GetType().Name, filename));
+                   }
+               }
+           }
+       }
     }
 }
