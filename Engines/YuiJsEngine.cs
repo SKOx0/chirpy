6ff145470a5d419d31303e061e1e67e6b106b650
ploufs <ploufs@localhost>
2012-04-21 15:42:14 -0400

    workitem=133 Fix bug : YUI doesn't compress JavaScript in a single line anymore

--- Engines/YuiJsEngine.cs
+++ Engines/YuiJsEngine.cs
@@ -20,18 +20,28 @@ namespace Zippy.Chirp.Engines
 
         public static string Minify(string fullFileName, string text, EnvDTE.ProjectItem projectItem,Yui.JsSettings jsSettings)
         {
+            string returnScript = string.Empty;
             var reporter = new EcmaScriptErrorReporter(fullFileName, projectItem);
             try
             {
+                // http://chirpy.codeplex.com/workitem/133
+                int lineBreakPosition = jsSettings.LineBreakPosition;
+                if (lineBreakPosition == 0)
+                {
+                    lineBreakPosition = -1;
+                }
+
                 // http://chirpy.codeplex.com/workitem/54
                 // http://chirpy.codeplex.com/workitem/60
                 var compressor = new JavaScriptCompressor(text, true, System.Text.Encoding.UTF8, System.Globalization.CultureInfo.InvariantCulture, false, reporter);
-                return compressor.Compress(jsSettings.IsObfuscateJavascript, jsSettings.PreserveAllSemiColons, jsSettings.DisableOptimizations, jsSettings.LineBreakPosition);
+                returnScript = compressor.Compress(jsSettings.IsObfuscateJavascript, jsSettings.PreserveAllSemiColons, jsSettings.DisableOptimizations, lineBreakPosition);
             }
-            catch (System.Exception)
+            catch (System.Exception eError)
             {
-                return "/* error */";
+                returnScript= string.Format("/* error = {0} */",eError.Message);
             }
+
+            return returnScript;
         }
 
         public override string Transform(string fullFileName, string text, EnvDTE.ProjectItem projectItem)
