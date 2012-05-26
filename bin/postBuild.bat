14137c13bb90c11182e20396df0cb5af36fdea05
ploufs <ploufs@localhost>
2012-05-26 12:54:38 -0400

    Create vsi for vs 2008,vs 2010 , vs 11

--- bin/postBuild.bat
+++ bin/postBuild.bat
@@ -1,10 +1,31 @@
 REM Merge assemblies
 ILMerge /out:"Chirpy.dll" "TempChirpy.dll" "EcmaScript.NET.modified.dll" "Yahoo.YUI.Compressor.dll" "dotless.Core.dll" "AjaxMin.dll" "Jurassic.dll" /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319
 REM Create VSI file
-del Chirpy.vsi
+
+REM VS (v9)
+del Chirpy-VisualStudio2008.vsi
 del Chirpy.zip
+del Zippy.AddIn
+copy Zippy09.AddIn Zippy.AddIn
 7za.exe u Chirpy.zip Chirpy.dll Chirpy.vscontent Zippy.AddIn "compiler.jar" "SassAndCoffee.Core.dll" "Microsoft.Dynamic.dll" "Microsoft.Scripting.dll" "Microsoft.Scripting.Metadata.dll" "IronRuby.dll" "IronRuby.Libraries.dll" "IronRuby.Libraries.Yaml.dll"
-rename Chirpy.zip Chirpy.vsi
+rename Chirpy.zip Chirpy-VisualStudio2008.vsi
+
+REM VS 2010 (v10)
+del Chirpy-VisualStudio2010.vsi
+del Chirpy.zip
+del Zippy.AddIn
+copy Zippy10.AddIn Zippy.AddIn
+7za.exe u Chirpy.zip Chirpy.dll Chirpy.vscontent Zippy.AddIn "compiler.jar" "SassAndCoffee.Core.dll" "Microsoft.Dynamic.dll" "Microsoft.Scripting.dll" "Microsoft.Scripting.Metadata.dll" "IronRuby.dll" "IronRuby.Libraries.dll" "IronRuby.Libraries.Yaml.dll"
+rename Chirpy.zip Chirpy-VisualStudio2010.vsi
+
+REM VS 11
+del Chirpy-VisualStudio2011.vsi
+del Chirpy.zip
+del Zippy.AddIn
+copy Zippy11.AddIn Zippy.AddIn
+7za.exe u Chirpy.zip Chirpy.dll Chirpy.vscontent Zippy.AddIn "compiler.jar" "SassAndCoffee.Core.dll" "Microsoft.Dynamic.dll" "Microsoft.Scripting.dll" "Microsoft.Scripting.Metadata.dll" "IronRuby.dll" "IronRuby.Libraries.dll" "IronRuby.Libraries.Yaml.dll"
+rename Chirpy.zip Chirpy-VisualStudio11.vsi
+
 REM copy file to setup project
 copy "Zippy.AddIn" "../Zippy.Setup.VS10/Zippy.AddIn"
 copy "Chirpy.dll" "../Zippy.Setup.VS10/Chirpy.dll"
